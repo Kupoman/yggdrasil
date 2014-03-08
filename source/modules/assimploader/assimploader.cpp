21f48ba71d7cbf424060c25dd2e0d9573785e12e
Daniel Stokes <kupomail@gmail.com>
2014-03-08 13:29:14 -0800

    AssimpLoader: Fixed pointer issues with lights
    The loader was making edits to a local light instead of the light in the
    component.

--- source/modules/assimploader/assimploader.cpp
+++ source/modules/assimploader/assimploader.cpp
@@ -24,34 +24,35 @@ int Ygg::AssimpLoaderSystem::LoadLights(size_t eoffset, std::vector<Entity> *equ
 	m_light_components.resize(coffset + ascene->mNumLights);
 	equeue->resize(eoffset + ascene->mNumLights);
 
-	AssimpLightComponent light_component;
+	AssimpLightComponent *light_component;
 	aiLight *alight;
 
 	for (unsigned int i = 0; i < ascene->mNumLights; i++) {
-		light_component = m_light_components[coffset + i];
+		light_component = &m_light_components[coffset + i];
 		alight = ascene->mLights[i];
 
-		light_component.light.angle_inner = alight->mAngleInnerCone;
-		light_component.light.angle_outer = alight->mAngleOuterCone;
+		light_component->light.angle_inner = alight->mAngleInnerCone;
+		light_component->light.angle_outer = alight->mAngleOuterCone;
 
-		light_component.light.att_constant = alight->mAttenuationConstant;
-		light_component.light.att_linear = alight->mAttenuationLinear;
-		light_component.light.att_quadratic = alight->mAttenuationQuadratic;
+		light_component->light.att_constant = alight->mAttenuationConstant;
+		light_component->light.att_linear = alight->mAttenuationLinear;
+		light_component->light.att_quadratic = alight->mAttenuationQuadratic;
 
-		light_component.light.color_ambient[0] = alight->mColorAmbient.r;
-		light_component.light.color_ambient[1] = alight->mColorAmbient.g;
-		light_component.light.color_ambient[2] = alight->mColorAmbient.b;
+		light_component->light.color_ambient[0] = alight->mColorAmbient.r;
+		light_component->light.color_ambient[1] = alight->mColorAmbient.g;
+		light_component->light.color_ambient[2] = alight->mColorAmbient.b;
 
-		light_component.light.color_diffuse[0] = alight->mColorDiffuse.r;
-		light_component.light.color_diffuse[1] = alight->mColorDiffuse.g;
-		light_component.light.color_diffuse[2] = alight->mColorDiffuse.b;
+		light_component->light.color_diffuse[0] = alight->mColorDiffuse.r;
+		light_component->light.color_diffuse[1] = alight->mColorDiffuse.g;
+		light_component->light.color_diffuse[2] = alight->mColorDiffuse.b;
 
-		light_component.light.color_specular[0] = alight->mColorSpecular.r;
-		light_component.light.color_specular[1] = alight->mColorSpecular.g;
-		light_component.light.color_specular[2] = alight->mColorSpecular.b;
+		light_component->light.color_specular[0] = alight->mColorSpecular.r;
+		light_component->light.color_specular[1] = alight->mColorSpecular.g;
+		light_component->light.color_specular[2] = alight->mColorSpecular.b;
 
 		(*equeue)[eoffset + i].name = std::string(ascene->mLights[i]->mName.C_Str());
-		(*equeue)[eoffset + i].components->insert(std::pair<int, void*>(id, &m_light_components));
+		(*equeue)[eoffset + i].components = new std::map<int, void*>();
+		(*equeue)[eoffset + i].components->insert(std::pair<int, void*>(id, light_component));
 	}
 
 	return ascene->mNumLights;
